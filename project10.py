c73b78c39e3214b946d4e0e9acfa0133d39d7352
JJglancek <minyon.hup@gmail.com>
2024-06-02 14:12:19 +0400

    Update project10.py

--- project10.py
+++ project10.py
@@ -1,8 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while i < 12:
-    if my_list[i] >= 0:
+    if my_list[i] > 0:
         print(my_list[i])
+        i = i + 1
+    elif my_list[i] == 0:
         i = i + 1
         continue
     else:
